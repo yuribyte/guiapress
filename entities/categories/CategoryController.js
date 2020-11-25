2c8a88f672d06982ae86fa03b3f83c07f1913072
Yuri Vitor <yuri.lins25@gmail.com>
2020-11-25 16:52:20 -0300

    cadastrando no banco novas categorias

--- entities/categories/CategoryController.js
+++ entities/categories/CategoryController.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const router = express()
 
+const slugify = require('slugify')
+
+const Category = require('../categories/Category')
+
 router.get('/', (req, res) => {
   res.render('categories')
 })
 
 router.get('/admin/form', (req, res) => {
   res.render('categories/form')
+})
+
+router.post('/', (req, res) => {
+  const { title } = req.body
+  if (title != undefined) {
+    Category.create({ title, slug: slugify(title) })
+      .then(() => res.redirect('/'))
+  } else {
+    res.redirect('/admin/form')
+  }
 })
 
 module.exports = router

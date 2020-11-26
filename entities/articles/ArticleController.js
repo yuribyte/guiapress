a9b9b533ef1eb41f9f158d24ffa3cc228d083460
Yuri Vitor <yuri.lins25@gmail.com>
2020-11-26 19:53:15 -0300

    criando artigos + validador + tinymce

--- entities/articles/ArticleController.js
+++ entities/articles/ArticleController.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const router = express()
 
-router.get('/', (req, res) => {
+const Category = require('../categories/Category')
+const Article = require('../articles/Article')
+const slugify = require('slugify')
+
+
+router.get('/list', (req, res) => {
   res.render('articles')
 })
 
-router.get('/admin/form', (req, res) => {
-  res.render('articles/form')
+router.get('/form', (req, res) => {
+  const props = { raw: true }
+  Category.findAll(props).then((categories) => {
+    res.render('articles/form', { categories })
+  })
+})
+
+// ! CREATE
+router.post('/', (req, res) => {
+  const { title, body, category } = req.body
+  if (title != undefined && body != undefined) {
+    Article.create({ title, body, categoryId: category, slug: slugify(title) })
+      .then(() => res.redirect('/article/list'))
+  } else {
+    res.redirect('/article/form') // ! Mantém na página
+  }
 })
 
 module.exports = router

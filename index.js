adda1786d7b3767d3936025098ca88254e2f796b
Yuri Vitor <yuri.lins25@gmail.com>
2020-11-30 13:20:40 -0300

    acessando detalhes do artigo

--- index.js
+++ index.js
@@ -42,27 +42,87 @@ app.use('/article', artController)
 
 app.get('/', (req, res) => {
   Article.findAll({
-    order: [['createdAt', 'DESC']],
+    order: [
+      ['createdAt', 'DESC']
+    ]
   }).then((articles) => {
     Category.findAll().then((categories) => {
-      res.render('index', { articles, categories, createdAt: formatDate(articles) })
+      res.render('index', {
+        articles,
+        categories,
+        createdAt: formatDateList(articles)
+      })
     })
   })
 })
 
+// ! DETAILS
+app.get('/article/:slug', (req, res) => {
+  const { slug } = req.params
+
+  Article.findOne({ where: { slug }, include: [{ model: Category }] })
+    .then((article) => {
+      if (article != undefined) {
+        Category.findAll().then((categories) => {
+          res.render('articles/details', {
+            article,
+            categories,
+            createdAt: formatDate(article),
+            updatedAt: formatDateHour(article)
+          })
+        })
+      } else {
+        res.redirect('/')
+      }
+    })
+    .catch((err) => {
+      res.redirect('/')
+    })
+})
+
 // Functions
 
-function formatDate(dados) {
+function formatDateList(dados) {
   for (let i = 0; i < dados.length; i++) {
-    const element = dados[i];
+    const element = dados[i]
     const date = new Date(Date.parse(element.createdAt))
     const day = date.getDate().toString()
-    const dayF = day.length == 1 ? '0' + day : day;
+    const dayF = day.length == 1 ? '0' + day : day
     const month = (date.getMonth() + 1).toString()
-    const monthF = month.length == 1 ? '0' + month : month;
+    const monthF = month.length == 1 ? '0' + month : month
     const yearF = date.getFullYear()
-    return `${dayF}/${monthF}/${yearF}`;
+    return `${dayF}/${monthF}/${yearF}`
   }
+}
+
+function formatDate(dados) {
+  const createdAt = dados.createdAt
+  const date = new Date(Date.parse(createdAt))
+  const day = date.getDate().toString()
+  const dayF = day.length == 1 ? '0' + day : day
+  const month = (date.getMonth() + 1).toString()
+  const monthF = month.length == 1 ? '0' + month : month
+  const yearF = date.getFullYear()
+  return `${dayF}/${monthF}/${yearF}`
+}
+
+function formatDateHour(dados) {
+  const updatedAt = dados.updatedAt
+  const date = new Date(Date.parse(updatedAt))
+
+  const day = date.getDate().toString()
+  const dayF = day.length == 1 ? '0' + day : day
+
+  const month = (date.getMonth() + 1).toString()
+  const monthF = month.length == 1 ? '0' + month : month
+
+  const yearF = date.getFullYear()
+
+  const hour = date.getHours().toString()
+  const minutes = date.getMinutes().toString()
+  const seconds = date.getSeconds().toString()
+
+  return `${dayF}/${monthF}/${yearF} às ${hour}:${minutes}:${seconds}`
 }
 
 // Start

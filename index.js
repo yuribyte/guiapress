e3708964ead22e62c1e603900673d399ffa8bdae
Yuri Vitor <yuri.lins25@gmail.com>
2020-11-30 12:14:44 -0300

    listando todos os artigos na pagina inicial

--- index.js
+++ index.js
@@ -8,25 +8,31 @@ const bodyParser = require('body-parser')
 const catController = require('./entities/categories/CategoryController')
 const artController = require('./entities/articles/ArticleController')
 
-const catModel = require('./entities/categories/Category')
-const artModel = require('./entities/articles/Article')
+const Category = require('./entities/categories/Category')
+const Article = require('./entities/articles/Article')
 
 // View Engine
+
 app.set('view engine', 'ejs')
 
 // Body Parser
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // Static Files
-app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.static(path.join(__dirname, '/public')))
 
 // Database
-connection.authenticate()
+
+connection
+  .authenticate()
   .then(() => {
-    console.log('connection ok')
-  }).catch((err) => {
-    console.log(err)
+    console.log('Connection stablished')
+  })
+  .catch((err) => {
+    console.warn(err)
   })
 
 // Routes
@@ -34,7 +40,31 @@ connection.authenticate()
 app.use('/category', catController)
 app.use('/article', artController)
 
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => {
+  Article.findAll({
+    order: [['createdAt', 'DESC']],
+  }).then((articles) => {
+    Category.findAll().then((categories) => {
+      res.render('index', { articles, categories, createdAt: formatDate(articles) })
+    })
+  })
+})
 
+// Functions
+
+function formatDate(dados) {
+  for (let i = 0; i < dados.length; i++) {
+    const element = dados[i];
+    const date = new Date(Date.parse(element.createdAt))
+    const day = date.getDate().toString()
+    const dayF = day.length == 1 ? '0' + day : day;
+    const month = (date.getMonth() + 1).toString()
+    const monthF = month.length == 1 ? '0' + month : month;
+    const yearF = date.getFullYear()
+    return `${dayF}/${monthF}/${yearF}`;
+  }
+}
+
+// Start
 
 app.listen(888, () => { })

b98b32fbad681158083dd02936f89b7ec21e8b26
Yuri Vitor <yuri.lins25@gmail.com>
2020-12-03 22:10:50 -0300

    correcao de bug para formatação de data + utilizando 'moment.js' para formatar de forma correta as datas

--- entities/articles/Article.js
+++ entities/articles/Article.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize')
 const connection = require('../../database/database')
 const Category = require('../categories/Category')
+const moment = require('moment')
 
 const Article = connection.define('articles', {
   title: {
@@ -18,13 +19,25 @@ const Article = connection.define('articles', {
   summary: {
     type: Sequelize.STRING,
     allowNull: true
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    get() {
+      return moment(this.getDataValue('createdAt')).format('DD/MM/YYYY HH:mm:ss');
+    }
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    get() {
+      return moment(this.getDataValue('updatedAt')).format('DD/MM/YYYY HH:mm:ss');
+    }
   }
-})  
+})
 
 Category.hasMany(Article)
 Article.belongsTo(Category)
 
-Article.sync({ force: false }).then(() => { }) 
-Category.sync({ force: false }).then(() => { }) 
+Article.sync({ force: false }).then(() => { })
+Category.sync({ force: false }).then(() => { })
 
 module.exports = Article
--- entities/categories/Category.js
+++ entities/categories/Category.js
@@ -9,9 +9,21 @@ const Category = connection.define('categories', {
   slug: {
     type: Sequelize.STRING,
     allowNull: false
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    get() {
+      return moment(this.getDataValue('createdAt')).format('DD/MM/YYYY HH:mm:ss');
+    }
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    get() {
+      return moment(this.getDataValue('updatedAt')).format('DD/MM/YYYY HH:mm:ss');
+    }
   }
 })
 
-Category.sync({ force: false }).then(() => { }) 
+Category.sync({ force: false }).then(() => { })
 
 module.exports = Category
